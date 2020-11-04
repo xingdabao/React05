c048e9aee36a87c8c1a30de8560f59a6b9e62e10
xingdabao <xy15532218813>
2020-11-04 20:57:27 +0800

    task

---
+++ precache-manifest.e47ac59ed712fe1e6d96bc012053c280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c2c6017651ff1ec4c10129290bfd98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3288a226487160b4357",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "6bb0e47c9b56ce467661",
+    "url": "/static/css/main.6924ece0.chunk.css"
+  },
+  {
+    "revision": "a3288a226487160b4357",
+    "url": "/static/js/2.647ca594.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.647ca594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb0e47c9b56ce467661",
+    "url": "/static/js/main.e4c89ada.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
